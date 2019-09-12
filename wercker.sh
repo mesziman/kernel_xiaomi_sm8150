e88dd2f48560043a21a2da742d3f75f358603b93
Gabor <meszaros.gabor.10k@gmail.com>
2019-09-12 21:43:10 +0200

    Update wercker.sh

--- wercker.sh
+++ wercker.sh
@@ -7,9 +7,6 @@ git clone --depth=30 https://android.googlesource.com/platform/prebuilts/gcc/lin
 cd /pipeline/build/root/toolchain/aarch64-linux-android-4.9 && git reset --hard 22f053ccdfd0d73aafcceff3419a5fe3c01e878b &&
 git clone --depth=30 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 &&
 cd /pipeline/build/root/toolchain/arm-linux-androideabi-4.9 && git reset --hard 42e5864a7d23921858ca8541d52028ff88acb2b6 &&
-git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 /pipeline/build/root/toolchain/gclang &&
-git clone https://github.com/PsyMan47/SnapDragonLLVM_6.0 /pipeline/build/root/toolchain/SnapDragonLLVM_6.0
-git clone -b 9.0 https://github.com/syberia-project/platform_prebuilts_build-tools /pipeline/build/root/toolchain/asd
-git clone -b 9.0 https://github.com/syberia-project/DragonTC /pipeline/build/root/toolchain/dtc
+git clone  --depth=3 https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 /pipeline/build/root/toolchain/gclang &&
 cd $LOFASZ
 bash builder.sh
